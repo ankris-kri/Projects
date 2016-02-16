7301ac389a17236482ffa2a783a9720e63fe2bdb
ankris-kri <agarwal.krishna240@gmail.com>
2016-02-16 18:54:07 +0530

    review comments written

--- Day2_redo/PhoneDirectory/PhoneDirectory/Form1.Designer.cs
+++ Day2_redo/PhoneDirectory/PhoneDirectory/Form1.Designer.cs
@@ -43,7 +43,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(19, 19);
+            this.label1.Location = new System.Drawing.Point(22, 261);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 0;
@@ -52,22 +52,22 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(110, 159);
+            this.dataGridView1.Location = new System.Drawing.Point(25, 106);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(263, 120);
             this.dataGridView1.TabIndex = 1;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(22, 44);
+            this.textBox1.Location = new System.Drawing.Point(25, 293);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(164, 20);
+            this.textBox1.Size = new System.Drawing.Size(131, 20);
             this.textBox1.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(214, 19);
+            this.label2.Location = new System.Drawing.Point(161, 261);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(44, 13);
             this.label2.TabIndex = 3;
@@ -75,14 +75,14 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(217, 43);
+            this.textBox2.Location = new System.Drawing.Point(164, 293);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(156, 20);
+            this.textBox2.Size = new System.Drawing.Size(131, 20);
             this.textBox2.TabIndex = 4;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(416, 40);
+            this.button1.Location = new System.Drawing.Point(111, 336);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 5;
@@ -93,7 +93,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(22, 87);
+            this.label3.Location = new System.Drawing.Point(22, 25);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(41, 13);
             this.label3.TabIndex = 6;
@@ -101,7 +101,7 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(22, 113);
+            this.textBox3.Location = new System.Drawing.Point(22, 56);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(164, 20);
             this.textBox3.TabIndex = 7;
@@ -109,7 +109,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(217, 110);
+            this.button2.Location = new System.Drawing.Point(201, 54);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 8;
@@ -121,7 +121,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(515, 291);
+            this.ClientSize = new System.Drawing.Size(307, 466);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label3);
--- Day2_redo/PhoneDirectory/PhoneDirectory/PhoneDirectoryRepository.cs
+++ Day2_redo/PhoneDirectory/PhoneDirectory/PhoneDirectoryRepository.cs
@@ -6,42 +6,35 @@ using System.Threading.Tasks;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+
 namespace PhoneDirectory
 {
+    //access modifiers in all class files
     class PhoneDirectoryRepository
     {
-        PhoneEntryDto result = new PhoneEntryDto();
-        ErrorDto error = new ErrorDto();
         string sqlConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["SqlConnection"].ConnectionString;
-        /*
-        public PhoneEntryDto Search(string input)
-        {
-            using (SqlConnection sqlConn = new SqlConnection(sqlConnectionString))
-            {
-                sqlConn.Open();
-                SqlDataAdapter _dataadapter = new SqlDataAdapter("select * from PhoneDirectory where Name like @input or Number like @input", sqlConn);
-                _dataadapter.SelectCommand.Parameters.AddWithValue("@input", "%" + input + "%");
-                DataTable t = new DataTable();
-                _dataadapter.Fill(t);
-                result.Table = t;
-                return result;
-            }
-        }
-        */
+
+        ErrorDto error = new ErrorDto();
+        
         public List<PhoneEntry> Search(string input)
         {
             List<PhoneEntry> list = new List<PhoneEntry>();
+
             using (SqlConnection sqlConn = new SqlConnection(sqlConnectionString))
             {
                 sqlConn.Open();
+
                 SqlDataAdapter _dataadapter = new SqlDataAdapter("select * from PhoneDirectory where Name like @input or Number like @input", sqlConn);
                 _dataadapter.SelectCommand.Parameters.AddWithValue("@input", "%" + input + "%");
+
                 DataTable table = new DataTable();
                 _dataadapter.Fill(table);
+
                 foreach (DataRow row in table.Rows)
                 {
                     list.Add(new PhoneEntry(row["Name"].ToString(),(long)Convert.ToDouble(row["Number"])));
                 }
+
                 return list;
             }
         }
@@ -52,12 +45,16 @@ namespace PhoneDirectory
                 using (SqlConnection sqlConn = new SqlConnection(sqlConnectionString))
                 {
                     sqlConn.Open();
+
                     var cmd = new SqlCommand("Insert into PhoneDirectory values(@name,@phone)");
                     cmd.Connection = sqlConn;
+
                     cmd.Parameters.AddWithValue("@name", phoneEntry.name);
                     cmd.Parameters.AddWithValue("@phone", phoneEntry.number);
+
                     cmd.ExecuteNonQuery();
-                    error.isError = false;
+
+                   // set it by default-- error.isError = true;
                     return error;
                 }
             }
