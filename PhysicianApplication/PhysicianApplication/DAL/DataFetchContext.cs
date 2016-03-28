38718fd7e40aa4b712d4bde88e446b786ba9873a
ankris-kri <agarwal.krishna240@gmail.com>
2016-03-28 12:31:27 +0530

    added contratcs

---
+++ PhysicianApplication/PhysicianApplication/DAL/DataFetchContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PhysicianApplication.Models
+{
+    public class DataFetchContext: DbContext
+    {
+        public virtual DbSet<Hospital> Hospitals { get; set; }
+        public virtual DbSet<Physician> Physicians { get; set; }
+        public virtual DbSet<Specialty> Specialties { get; set; }
+
+        //public System.Data.Entity.DbSet<PhysicianApplication.Models.DataToView> DataToViews { get; set; }
+    }
+}
